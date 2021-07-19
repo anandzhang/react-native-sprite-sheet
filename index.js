74d527867d1991637967423081f3286e43d2853a
anandzhang <hi@anandzhang.com>
2021-07-19 16:40:29 +0800

    支持百分比宽高样式

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Image, View } from 'react-native'
 
-const SpriteImage = ({ source, data, name, width, height }) => {
+const SpriteImage = ({ source, data, name, style }) => {
+    // 从图集的 JSON 中获取数据
     const { size, frames } = data
-    // 精灵图集中该图片
-    const { x, y, width: frameWidth, height: frameHeight } = frames[name]
+    
+    // 按名称获取精灵图
+    const frame = frames[name]
+    const { x, y, width: frameWidth, height: frameHeight } = frame
+
     // 图片真实宽高与需渲染宽高的缩放比例
-    const scaleX = width / frameWidth
-    const scaleY = height / frameHeight
+    const [scaleX, setScaleX] = useState(0)
+    const [scaleY, setScaleY] = useState(0)
+    const onLayout = ({ nativeEvent: { layout } }) => {
+        const { width, height } = layout
+        setScaleX(width / frameWidth)
+        setScaleY(height / frameHeight)
+    }
+
     // 缩放的锚点在中心，需要移动到左上角
     // -(1 - scaleX) * frameWidth / 2
     const translateX = (scaleX - 1) * frameWidth / 2
     const translateY = (scaleY - 1) * frameHeight / 2
     return (
-        <View style={{ width, height }}>
+        <View
+            style={style}
+            onLayout={onLayout}
+        >
             <View
                 style={{
                     width: frameWidth,
